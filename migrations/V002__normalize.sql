f3a0f1e923df8ed381a20b2dc44aa92484882872
Tim <herew26@gmail.com>
2026-03-22 22:53:50 +0300

    Fix normalization migration

--- migrations/V002__normalize.sql
+++ migrations/V002__normalize.sql
@@ -7,9 +7,9 @@ UPDATE orders o
 DROP TABLE IF EXISTS orders_date;
 
 ALTER TABLE product
-    ADD COLUMN IF NOT EXISTS picture_url varchar(255);
+    ADD COLUMN IF NOT EXISTS price double precision;
 UPDATE product p
-    SET picture_url = pi.picture_url
+    SET price = pi.price
     FROM product_info pi
     WHERE p.id = pi.product_id;
 DROP TABLE IF EXISTS product_info;
